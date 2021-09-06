401d1cd6e68455838113d59efe76f8f89ef65764
Gerardo Abraham <geoabraham@tuta.io>
2021-09-06 16:31:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ def get_servers():
     # fetching from the database
     session = Session()
     server_objects = session.query(Server).all()
-    
+
     # transforming into JSON-serializable objects
     schema = ServerSchema(many=True)
     servers = schema.dump(server_objects)
-    
+
     # serializing as JSON
     session.close()
     return jsonify(servers)
@@ -28,16 +28,18 @@ def get_servers():
 @app.route("/servers", methods=["POST"])
 def add_server():
     # mount server object
-    posted_server = ServerSchema(only=("customer_id", "hostname", "os", "ram", "cpu")).load(request.get_json())
+    posted_server = ServerSchema(
+        only=("customer_id", "hostname", "os", "ram", "cpu")
+    ).load(request.get_json())
     server = Server(**posted_server, created_by="HTTP post request")
-    
+
     # persist server
     session = Session()
     session.add(server)
     session.commit()
-    
+
     # return created server
     new_server = ServerSchema().dump(server)
     session.close()
-    
+
     return jsonify(new_server), 201
