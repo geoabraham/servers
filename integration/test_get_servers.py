7eddd51d9029af3f33ee7b94ed9b0ab6f2574f00
GonzaloSaad <saad.gonzalo.ale@gmail.com>
2021-09-22 18:41:02 -0300

    test: add GET & POST scenarios

--- integration/test_get_servers.py
+++ integration/test_get_servers.py
@@ -1,17 +1,43 @@
 from http import HTTPStatus
 
+import pytest
 import requests
 
 
 # Scenarios for GET /v1/servers
 # Scenarios 01: Success
-def test_get_servers_success():
-    # Given
-    customer_id = "218e7c3b-8adc-4104-9691-38e06e6efcd9"
-    query_params = {"customer_id": customer_id}
-
+# Scenarios 02: Error - Invalid Customer ID
+def test_get_servers_success(customer_id, servers_client):
     # When
-    response = requests.get("http://localhost:5000/servers", params=query_params)
+    response = servers_client.get_servers(customer_id)
 
     # Then
     assert response.status_code == HTTPStatus.OK
+    body = response.json()
+
+    data = body["data"]
+    meta = body["meta"]
+    assert len(data) == meta["count"]
+
+
+@pytest.mark.parametrize(
+    "invalid_customer_id",
+    [
+        "not_uuid",
+        "",
+        123,
+    ],
+)
+def test_get_servers_invalid_customer_id(invalid_customer_id, servers_client):
+    # When
+    response = servers_client.get_servers(invalid_customer_id)
+
+    # Then
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    body = response.json()
+
+    # Assert over the error body
+    errors = body["errors"]
+    assert len(errors) == 1
+
+    assert errors[0]["code"] == "validation_error"
